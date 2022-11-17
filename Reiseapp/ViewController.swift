54e940f4a77c7fb29612940751298744c2da34e1
Stefan <103416259+Stefan6768@users.noreply.github.com>
2022-11-17 08:51:54 +0100

    Initial Commit

---
+++ Reiseapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reiseapp
+//
+//  Created by Stefan Schreiber on 17.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
